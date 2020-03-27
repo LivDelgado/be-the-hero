69957c910bd0701c68a1a051940b679f2beefef3
Lívia Delgado <livia.delgado.ac@gmail.com>
2020-03-27 17:36:06 -0300

    Separação de arquivo de rotas

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 const express = require('express'); // importação do express
+const routes = require('./routes');
 
 const app = express(); // criando a aplicação
+
 app.use(express.json());
+app.use(routes);
 
 const port = 3333;
-
-app.get('/', (request, response) => {
-    return response.json({
-        evento: "Semana OmniStack 11.0",
-        aluno: "Lívia Delgadado"
-    });
-});
-
 app.listen(port); // configurando a porta do servidor da aplicação
 console.info("Server listening at port " + port);
